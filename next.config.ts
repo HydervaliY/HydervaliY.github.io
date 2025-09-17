ebc7f95eeaad8215abd750738634e9f8c2478b1b
Hydervali Yalamkur <hydervaliyalamkur@Hydervalis-MBP.wifi.gsu.edu>
2025-09-17 17:10:02 -0400

    profile exported to node environment

--- next.config.ts
+++ next.config.ts
@@ -4,5 +4,8 @@ import createMDX from '@next/mdx'
 const withMDX = createMDX()
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  output: 'export',
+      basePath: process.env.NODE_ENV === 'production' ? '/HydervaliY.github.io' : '',
+      assetPrefix: process.env.NODE_ENV === 'production' ? '/HydervaliY.github.io/' : ''
 }
 export default withMDX(nextConfig)
